a92bfe92b8fffb29ec8eaa1be3568d0a1cd7e29e
root <root@ip-172-26-9-31.ap-southeast-1.compute.internal>
2020-01-26 00:17:32 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Netflix DNS Resolver
+
+Link : [http://let.my.id/netflix/hosts.txt](http://let.my.id/netflix/hosts.txt)
+
+
+## Add domain lists
+Add the domains to `domain.txt`
+
+## Add CDN lists
+Add the CND domains to `cdn.txt`
+
+
+`Format : link cdn + prefix x number`
+#### Example 1 :
+```
+occ-0-64-325.1.nflxso.net
+occ-1-64-325.1.nflxso.net
+.....
+occ-100-64-325.1.nflxso.net
+```
+prefix: 1 number
+
+write: `ipv4-c -sin001-ix.1.oca.nflxvideo.net 1` to `cdn.txt` 
+
+#### Example 2 :
+```
+ipv4-c001-sin001-ix.1.oca.nflxvideo.net
+ipv4-c002-sin001-ix.1.oca.nflxvideo.net
+...
+ipv4-c100-sin001-ix.1.oca.nflxvideo.net
+```
+prefix : 3 number
+
+write: `ipv4-c -sin001-ix.1.oca.nflxvideo.net 3` to `cdn.txt` 
+
---
+++ dns-resolver.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+source  config
+
+if [ ! -f $FILENAME ]; then
+	rm -rf $FILENAME
+fi
+
+echo "# Author : Galih < me@galihyoungest.web.id >" > $FILENAME
+echo "# http://let.my.id/netflix/hosts.txt" >> $FILENAME
+echo "# netflix-dns" >> $FILENAME
+
+RESOLVER() {
+	for ip in `dig $DOMAIN +short`; do
+		if [ $? = 0 ]; then
+			if  [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+				echo $ip $DOMAIN >> $FILENAME
+			fi
+		fi
+	done
+}
+
+DOMAIN(){
+	for dns in `cat $DOMAIN`; do
+		DOMAIN=$dns
+		RESOLVER
+	done
+}
+
+CDN() {
+	cat $CDN | grep -v "#" | while read i; do
+	PREFIXNUMBER=`echo $i | cut -d ' ' -f3`
+	PREFIX1=`echo $i | cut -d ' ' -f1`
+	PREFIX2=`echo $i | cut -d ' ' -f2`
+	if [ $PREFIXNUMBER == 1 ]; then
+		for j in {1..2};do
+			DOMAIN="$PREFIX1$j$PREFIX2"
+			RESOLVER
+		done
+	elif [ $PREFIXNUMBER == 2 ]; then
+		for j in {01..02};do
+			DOMAIN="$PREFIX1$j$PREFIX2"
+			RESOLVER
+		done
+	elif [ $PREFIXNUMBER == 3 ]; then
+		for j in {001..002};do
+			DOMAIN="$PREFIX1$j$PREFIX2"
+			RESOLVER
+		done
+	else
+		echo "out of scope"
+	fi
+	done
+}
+
+main() {
+	DOMAIN
+	CDN
+}
+
+main
+
+echo "# Last update : `date`" >> $FILENAME
